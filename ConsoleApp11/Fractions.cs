f72fc3d585bfdc3f17c819ebd916e50e335dd498
joinThe <stadnikgleb@gmail.com>
2019-06-10 20:55:57 +0300

    Add project files.

---
+++ ConsoleApp11/Fractions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Fractions
+    {
+        private double a;
+        private double b;
+
+
+        public Fractions(int v1, int v2)
+        {
+            this.a = v1;
+            this.B = v2;
+        }
+
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value==0?1:value; }
+
+        public override string ToString()
+        {
+            return "  "+A + "\n ----- \n  "+B;
+        }
+
+       
+        
+
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fractions fractions = new Fractions(5, 12);
+            Console.WriteLine(fractions);
+            Fractions fractions2 = new Fractions(5, 0);
+            Console.WriteLine( fractions2);
+        }
+    }
+}
